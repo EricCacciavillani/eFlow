Fixed directory problem for installation of packages and changed correlation mapping to handle no numerical values in a dataframe.

eflow/data_analysis/feature_analysis.py
@@ -187,53 +187,57 @@ class FeatureAnalysis(DataAnalysis):
                                    f"{dataset_name}" + "/_Extras/Statistics")
 
             corr_df = feature_correlation_table(df)
-            self.save_table_as_plot(df=df,
-                                    table=corr_df,
-                                    show_index=True,
-                                    format_float_pos=7,
-                                    df_features=self.__df_features,
-                                    filename="Correlation Table",
-                                    sub_dir=f"{dataset_name}" + "/_Extras/Statistics",
-                                    dataframe_snapshot=False,
-                                    suppress_runtime_errors=suppress_runtime_errors,
-                                    meta_data=False)
+
+            # Ensures that the correlation dataframe has numerical values
+            if len(corr_df):
+
+                self.save_table_as_plot(df=df,
+                                        table=corr_df,
+                                        show_index=True,
+                                        format_float_pos=7,
+                                        df_features=self.__df_features,
+                                        filename="Correlation Table",
+                                        sub_dir=f"{dataset_name}" + "/_Extras/Statistics",
+                                        dataframe_snapshot=False,
+                                        suppress_runtime_errors=suppress_runtime_errors,
+                                        meta_data=False)
 
 
-            corr_df = average_feature_correlation_table(df)
-            self.save_table_as_plot(df=df,
-                                    table=corr_df,
-                                    show_index=True,
-                                    format_float_pos=7,
-                                    df_features=self.__df_features,
-                                    filename="Average Correlation Table",
-                                    sub_dir=f"{dataset_name}" + "/_Extras/Statistics",
-                                    dataframe_snapshot=False,
-                                    suppress_runtime_errors=suppress_runtime_errors,
-                                    meta_data=False)
+                corr_df = average_feature_correlation_table(df)
+                self.save_table_as_plot(df=df,
+                                        table=corr_df,
+                                        show_index=True,
+                                        format_float_pos=7,
+                                        df_features=self.__df_features,
+                                        filename="Average Correlation Table",
+                                        sub_dir=f"{dataset_name}" + "/_Extras/Statistics",
+                                        dataframe_snapshot=False,
+                                        suppress_runtime_errors=suppress_runtime_errors,
+                                        meta_data=False)
 
-            # Init color ranking fo plot
-            # Ref: http://tinyurl.com/ydgjtmty
-            plt.figure(figsize=(13, 10))
-            pal = sns.color_palette("GnBu_d", len(corr_df["Average Correlations"]))
-            rank = np.array(corr_df["Average Correlations"]).argsort().argsort()
-            ax = sns.barplot(y=corr_df.index.tolist(), x=corr_df["Average Correlations"],
-                             palette=np.array(pal[::-1])[rank])
-            plt.xticks(rotation=0, fontsize=15)
-            plt.yticks(fontsize=15)
-            plt.xlabel("Features", fontsize=20, labelpad=20)
-            plt.ylabel("Correlation Average", fontsize=20, labelpad=20)
-            plt.title("Average Feature Correlation", fontsize=15)
-            self.save_plot(df=df,
-                           df_features=self.__df_features,
-                           filename="Average Correlation Rank Graph",
-                           sub_dir=f"{dataset_name}" + "/_Extras/Statistics",
-                           dataframe_snapshot=False,
-                           suppress_runtime_errors=suppress_runtime_errors,
-                           meta_data=False)
+                # Init color ranking fo plot
+                # Ref: http://tinyurl.com/ydgjtmty
+                plt.figure(figsize=(13, 10))
+                pal = sns.color_palette("GnBu_d", len(corr_df["Average Correlations"]))
+                rank = np.array(corr_df["Average Correlations"]).argsort().argsort()
+                ax = sns.barplot(y=corr_df.index.tolist(), x=corr_df["Average Correlations"],
+                                 palette=np.array(pal[::-1])[rank])
+                plt.xticks(rotation=0, fontsize=15)
+                plt.yticks(fontsize=15)
+                plt.xlabel("Features", fontsize=20, labelpad=20)
+                plt.ylabel("Correlation Average", fontsize=20, labelpad=20)
+                plt.title("Average Feature Correlation", fontsize=15)
+                self.save_plot(df=df,
+                               df_features=self.__df_features,
+                               filename="Average Correlation Rank Graph",
+                               sub_dir=f"{dataset_name}" + "/_Extras/Statistics",
+                               dataframe_snapshot=False,
+                               suppress_runtime_errors=suppress_runtime_errors,
+                               meta_data=False)
 
-            plt.close("all")
+                plt.close("all")
 
-            del corr_df
+                del corr_df
 
             # Set to true to represent the function call was made with perform
             self.__called_from_perform = True