ca237c5abbcba48952e42d52cba74ed4fbfbd187
EricCacciavillani <EricCacciavillani@github.com>
2020-05-15 20:12:19 -0400

    Forget the pillow package in setup file

--- eflow/data_pipeline_segments/__init__.py
+++ eflow/data_pipeline_segments/__init__.py
@@ -1,4 +1,3 @@
 from .feature_transformer import FeatureTransformer
-from .type_fixer import TypeFixer
 from .data_encoder import DataEncoder
 from .feature_data_cleaner import FeatureDataCleaner
--- setup.py
+++ setup.py
@@ -1,13 +1,27 @@
-
 import setuptools
+
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
-MAIN_PACKAGES = [i.strip() for i in open("main_packages.txt").readlines()]
-
 setuptools.setup(
      name='eflow',
-     version='0.2.00',
+     version='0.2.42',
+     install_requires=['numpy',
+                       'matplotlib',
+                       'scipy',
+                       'seaborn',
+                       'jupyterlab',
+                       'pandas',
+                       'missingno',
+                       'sklearn',
+                       'kneed',
+                       'tqdm',
+                       'Pillow',
+                       'python-dateutil',
+                       'scikit-plot',
+                       'ipywidgets',
+                       'nltk',
+                       'ipython_blocking'],
      scripts=['init_env'],
      author="Eric Cacciavillani",
      author_email="eric.cacciavillani@gmail.com",
@@ -16,7 +30,6 @@ setuptools.setup(
      long_description_content_type="text/markdown",
      url="https://github.com/EricCacciavillani/eFlow",
      packages=setuptools.find_packages(),
-     install_requires=MAIN_PACKAGES,
      classifiers=[
          "Programming Language :: Python :: 3.7",
          "License :: OSI Approved :: MIT License",
